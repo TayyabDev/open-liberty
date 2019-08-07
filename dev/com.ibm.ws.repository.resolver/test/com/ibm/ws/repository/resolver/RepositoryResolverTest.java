03b80b233c0fcb0d3042c24215f8f85f6140a0aa
Tayyab Waqar <tayyab.waqar@ibm.com>
2019-08-07 14:45:47 -0400

    Add buggy test case to resolver test

--- dev/com.ibm.ws.repository.resolver/test/com/ibm/ws/repository/resolver/RepositoryResolverTest.java
+++ dev/com.ibm.ws.repository.resolver/test/com/ibm/ws/repository/resolver/RepositoryResolverTest.java
@@ -19,6 +19,7 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.core.AnyOf.anyOf;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -34,6 +35,7 @@ import org.junit.Test;
 
 import com.ibm.ws.kernel.feature.provisioning.ProvisioningFeatureDefinition;
 import com.ibm.ws.repository.common.enums.ResourceType;
+import com.ibm.ws.repository.common.enums.Visibility;
 import com.ibm.ws.repository.resolver.RepositoryResolutionException.MissingRequirement;
 import com.ibm.ws.repository.resolver.internal.kernel.KernelResolverEsa;
 import com.ibm.ws.repository.resources.EsaResource;
@@ -155,6 +157,233 @@ public class RepositoryResolverTest {
         assertThat(distanceMap, hasEntry("com.example.featureC", 0));
         assertThat(distanceMap, hasEntry("com.example.featureD", 1));
         assertThat(distanceMap.entrySet(), hasSize(2));
+    }
+
+//    @Test
+//    public void simpleTest() throws RepositoryResolutionException {
+//        EsaResourceWritable featurePw = WritableResourceFactory.createEsa(null);
+//        featurePw.setProvideFeature("com.ibm.websphere.appserver.passwordUtilities-1.0");
+//        featurePw.addRequireFeatureWithTolerates("com.ibm.websphere.appserver.javax.connector-1.6", Collections.<String> emptyList());
+//
+//        FeatureResolver resolver = new FeatureResolverImpl();
+//        KernelResolverRepository repo = new KernelResolverRepository(null, null);
+//
+//
+//        Result result = resolver.resolveFeatures(repo, Arrays.asList("com.example.featureB"), Collections.<String> emptySet(), false);
+//
+//        assertThat(result, is(result().withResolvedFeatures("com.example.featureA-1.1", "com.example.featureB")));
+//
+//    }
+
+    /*
+     * Dependency map
+     * com.ibm.websphere.appserver.passwordUtilities-1.0
+     * --> com.ibm.websphere.appserver.javax.connector-1.6
+     * ----> com.ibm.websphere.appserver.javax.connector.internal-1.6
+     * com.ibm.websphere.appserver.jpa-2.1
+     * --> com.ibm.websphere.appserver.transaction-1.2
+     * ----> com.ibm.websphere.appserver.javax.connector.internal-1.7
+     */
+
+    @Test
+    public void myTestReal() throws RepositoryResolutionException {
+        EsaResourceWritable featurePw = WritableResourceFactory.createEsa(null);
+        featurePw.setProvideFeature("com.ibm.websphere.appserver.passwordUtilities-1.0");
+        featurePw.addRequireFeatureWithTolerates("com.ibm.websphere.appserver.javax.connector-1.6", Collections.<String> emptyList());
+        featurePw.setVisibility(Visibility.PUBLIC);
+
+        EsaResourceWritable featureJavax = WritableResourceFactory.createEsa(null);
+        featureJavax.setProvideFeature("com.ibm.websphere.appserver.javax.connector-1.6");
+        featureJavax.addRequireFeatureWithTolerates("com.ibm.websphere.appserver.javax.connector.internal-1.6", Collections.<String> emptyList());
+        featureJavax.setVisibility(Visibility.PUBLIC);
+
+        EsaResourceWritable featureJavaxInteral16 = WritableResourceFactory.createEsa(null);
+        featureJavaxInteral16.setProvideFeature("com.ibm.websphere.javax.connector.internal-1.6");
+        featureJavaxInteral16.setVisibility(Visibility.PUBLIC);
+
+        EsaResourceWritable featureJpa = WritableResourceFactory.createEsa(null);
+        featureJpa.setProvideFeature("com.ibm.websphere.appserver.jpa-2.1");
+        featureJpa.addRequireFeatureWithTolerates("com.ibm.websphere.appserver.transaction-1.2", Collections.<String> emptyList());
+        featureJpa.setVisibility(Visibility.PUBLIC);
+
+        EsaResourceWritable featureTransaction = WritableResourceFactory.createEsa(null);
+        featureTransaction.setProvideFeature("com.ibm.websphere.appserver.transaction-1.2");
+        featureTransaction.addRequireFeatureWithTolerates("com.ibm.websphere.appserver.javax.connector.internal-1.7", Collections.<String> emptyList());
+        featureTransaction.setVisibility(Visibility.PUBLIC);
+
+        EsaResourceWritable featureJavaxInteral17 = WritableResourceFactory.createEsa(null);
+        featureJavaxInteral17.setProvideFeature("com.ibm.websphere.javax.connector.internal-1.7");
+        featureJavaxInteral17.setVisibility(Visibility.PUBLIC);
+
+        RepositoryResolver resolver = testResolver().withFeature(featurePw, featureJavax, featureJavaxInteral16, featureJpa, featureTransaction, featureJavaxInteral17)
+                                                    .build();
+
+        resolver.resolveAsSet(Arrays.asList(featurePw.getProvideFeature(), featureJpa.getProvideFeature()));
+        //List<List<RepositoryResource>> ret = (List) resolver.resolveAsSet(Arrays.asList(featurePw.getProvideFeature(), featureJpa.getProvideFeature()));
+        //System.out.println(ret.get(0).size());
+        //assertTrue(ret.get(0).containsAll(Arrays.asList(featurePw, featureJavax, featureJavaxInteral17))); // this should contain passwordUtilities, javax connector 1.6, and javax connector internal 1.7
+
+    }
+
+    /*
+     * Dependency map
+     * Feature B -> Feature A-1,0, tolerates 1.1
+     * Feature C -> Feature A-1.1
+     * Expected Output: Feature B, Feature C, Feature A-1.1
+     */
+    @Test
+    public void kerneltest() throws RepositoryResolutionException {
+        EsaResourceWritable featureA10 = WritableResourceFactory.createEsa(null);
+        featureA10.setProvideFeature("com.example.featureA-1.0");
+        featureA10.setVisibility(Visibility.PUBLIC);
+        featureA10.setSingleton("true");
+
+        EsaResourceWritable featureA11 = WritableResourceFactory.createEsa(null);
+        featureA11.setProvideFeature("com.example.featureA-1.1");
+        featureA11.setVisibility(Visibility.PUBLIC);
+        featureA11.setSingleton("true");
+
+        EsaResourceWritable featureB = WritableResourceFactory.createEsa(null);
+        featureB.setProvideFeature("com.example.featureB");
+        featureB.setVisibility(Visibility.PUBLIC);
+        featureB.addRequireFeatureWithTolerates("com.example.featureA-1.0", Arrays.asList("1.1"));
+
+        EsaResourceWritable featureC = WritableResourceFactory.createEsa(null);
+        featureC.setProvideFeature("com.example.featureC");
+        featureC.setVisibility(Visibility.PUBLIC);
+        featureC.addRequireFeatureWithTolerates("com.example.featureA-1.1", Collections.<String> emptyList());
+
+        RepositoryResolver resolver = testResolver().withFeature(featureA10, featureA11, featureB, featureC).build();
+        resolver.resolveAsSet(Arrays.asList(featureB.getProvideFeature(), featureC.getProvideFeature()));
+
+        for (String name : resolver.resolvedFeatures.keySet()) {
+            System.out.println(name);
+        }
+
+    }
+
+    // Dependency map
+    // Feature X -> Feature I 1.0
+    // Feature Y -> Feature I 1.1 tolerate 1.0
+
+    @SuppressWarnings("unchecked")
+    @Test
+    public void myTestNewXandY() throws RepositoryResolutionException {
+
+        EsaResourceWritable featureX = WritableResourceFactory.createEsa(null);
+        featureX.setProvideFeature("com.example.featureX-1.0");
+        featureX.addRequireFeatureWithTolerates("com.example.featureI-1.0", Collections.<String> emptyList());
+        featureX.setVisibility(Visibility.PUBLIC);
+
+        EsaResourceWritable featureY = WritableResourceFactory.createEsa(null);
+        featureY.setProvideFeature("com.example.featureY-1.0");
+        featureY.addRequireFeatureWithTolerates("com.example.featureI-1.1", Arrays.asList("1.0"));
+        featureY.setVisibility(Visibility.PUBLIC);
+
+        EsaResourceWritable featureI10 = WritableResourceFactory.createEsa(null);
+        featureI10.setProvideFeature("com.example.featureI-1.0");
+        featureI10.setVisibility(Visibility.PUBLIC);
+        featureI10.setSingleton("true");
+
+        EsaResourceWritable featureI11 = WritableResourceFactory.createEsa(null);
+        featureI11.setProvideFeature("com.example.featureI-1.1");
+        featureI11.setVisibility(Visibility.PUBLIC);
+        featureI11.setSingleton("true");
+
+        RepositoryResolver resolver = testResolver().withFeature(featureX, featureY, featureI10, featureI11).build();
+        resolver.resolveAsSet(Arrays.asList(featureX.getProvideFeature(), featureY.getProvideFeature()));
+
+        for (String name : resolver.resolvedFeatures.keySet()) {
+            System.out.println(name);
+        }
+    }
+
+    // Dependency map
+    // Feature A -> Feature X -> Feature I 1.0
+    // Feature B -> Feature Y -> Feature I 1.1 tolerate 1.0
+    @Test
+    public void myTestNew() throws RepositoryResolutionException {
+        EsaResourceWritable featureA = WritableResourceFactory.createEsa(null);
+        featureA.setProvideFeature("com.example.featureA-1.0");
+        featureA.addRequireFeatureWithTolerates("com.example.featureX-1.0", Collections.<String> emptyList());
+        featureA.setVisibility(Visibility.PUBLIC);
+
+        EsaResourceWritable featureB = WritableResourceFactory.createEsa(null);
+        featureB.setProvideFeature("com.example.featureB-1.0");
+        featureB.addRequireFeatureWithTolerates("com.example.featureY-1.0", Collections.<String> emptyList());
+        featureB.setVisibility(Visibility.PUBLIC);
+
+        EsaResourceWritable featureX = WritableResourceFactory.createEsa(null);
+        featureX.setProvideFeature("com.example.featureX-1.0");
+        featureX.addRequireFeatureWithTolerates("com.example.featureI-1.0", Collections.<String> emptyList());
+        featureX.setVisibility(Visibility.PUBLIC);
+
+        EsaResourceWritable featureY = WritableResourceFactory.createEsa(null);
+        featureY.setProvideFeature("com.example.featureY-1.0");
+        featureY.addRequireFeatureWithTolerates("com.example.featureI-1.1", Arrays.asList("1.0"));
+        featureY.setVisibility(Visibility.PUBLIC);
+
+        EsaResourceWritable featureI10 = WritableResourceFactory.createEsa(null);
+        featureI10.setProvideFeature("com.example.featureI-1.0");
+        featureI10.setVisibility(Visibility.PUBLIC);
+        featureI10.setSingleton("true");
+
+        EsaResourceWritable featureI11 = WritableResourceFactory.createEsa(null);
+        featureI11.setProvideFeature("com.example.featureI-1.1");
+        featureI11.setVisibility(Visibility.PUBLIC);
+        featureI11.setSingleton("true");
+
+        RepositoryResolver resolver = testResolver().withFeature(featureA, featureB, featureX, featureY, featureI10, featureI11).build();
+        resolver.resolveAsSet(Arrays.asList(featureA.getProvideFeature(), featureB.getProvideFeature()));
+
+        for (String name : resolver.resolvedFeatures.keySet()) {
+            System.out.println(name);
+        }
+
+//        List<List<RepositoryResource>> ret = (List) resolver.resolveAsSet(Arrays.asList(featureA.getProvideFeature(), featureB.getProvideFeature()));
+//        System.out.println(ret.size());
+//        //  assertTrue(ret.get(0).containsAll(Arrays.asList(featureA, featureX, featureI10))); // this should contain featureA, featureX, featureI-1.0
+//        //    assertTrue(ret.get(1).containsAll(Arrays.asList(featureB, featureY, featureI10))); // this should contain featureB, featureY, featureI-1.0
+
+    }
+
+    @Test
+    public void myTestOld() throws RepositoryResolutionException {
+        EsaResourceWritable featureA = WritableResourceFactory.createEsa(null);
+        featureA.setProvideFeature("com.example.featureA-1.0");
+        featureA.addRequireFeatureWithTolerates("com.example.featureX-1.0", Collections.<String> emptyList());
+
+        EsaResourceWritable featureB = WritableResourceFactory.createEsa(null);
+        featureB.setProvideFeature("com.example.featureB-1.0");
+        featureB.addRequireFeatureWithTolerates("com.example.featureY-1.0", Collections.<String> emptyList());
+
+        EsaResourceWritable featureX = WritableResourceFactory.createEsa(null);
+        featureX.setProvideFeature("com.example.featureX-1.0");
+        featureX.addRequireFeatureWithTolerates("com.example.featureI-1.0", Collections.<String> emptyList());
+
+        EsaResourceWritable featureY = WritableResourceFactory.createEsa(null);
+        featureY.setProvideFeature("com.example.featureY-1.0");
+        featureY.addRequireFeatureWithTolerates("com.example.featureI-1.1", Arrays.asList("1.0"));
+
+        EsaResourceWritable featureI10 = WritableResourceFactory.createEsa(null);
+        featureI10.setProvideFeature("com.example.featureI-1.0");
+        featureI10.setSingleton("true");
+
+        EsaResourceWritable featureI11 = WritableResourceFactory.createEsa(null);
+        featureI11.setProvideFeature("com.example.featureI-1.1");
+        featureI11.setSingleton("true");
+
+        RepositoryResolver resolver = testResolver().withResolvedFeature(featureA, featureB).build();
+        assertTrue(resolver.createInstallList(featureA.getProvideFeature()).containsAll(Arrays.asList(featureA, featureX, featureI10)));
+        assertTrue(resolver.createInstallList(featureB.getProvideFeature()).containsAll(Arrays.asList(featureB, featureY, featureI10)));
+
+//
+//        List<List<RepositoryResource>> ret = (List) resolver.resolveAsSet(Arrays.asList(featureA.getProvideFeature(), featureB.getProvideFeature()));
+//
+//
+//        assertTrue(ret.get(0).containsAll(Arrays.asList(featureA, featureX, featureI10))); // this should contain featureA, featureX, featureI-1.0
+//        assertTrue(ret.get(1).containsAll(Arrays.asList(featureB, featureY, featureI11))); // this should contain featureB, featureY, featureI-1.1
+//    }
     }
 
     @Test
